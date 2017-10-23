6f48851ae4dcddb3017d361667a71a30cbfa3286
shanjiaxiang <1135630810@qq.com>
2017-10-23 19:06:37 +0800

    201710231906

---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Shanjiaxiang on 2017/10/23.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temprature;
+    @SerializedName("cond")
+    public More more;
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
