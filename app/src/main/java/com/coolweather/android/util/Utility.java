704d138229ddd24e5503a71f3ec40f4c0927ede4
shanjiaxiang <1135630810@qq.com>
2017-10-25 18:07:43 +0800

    天气显示，手动更改城市

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -72,7 +72,7 @@ public class Utility {
                     JSONObject countyObject = allCounties.getJSONObject(i);
                     County county = new County();
                     county.setCountyName(countyObject.getString("name"));
-                    county.setWeatherId(countyObject.getInt("id"));
+                    county.setWeatherId(countyObject.getString("id"));
                     county.setCityId(cityId);
                     county.save();
                 }
